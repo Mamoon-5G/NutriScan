a903e227a8c4d033ef9144831f13eedf6b5528a1
Siddiqui Mamoon Ahmad <siddiquimamoon2004ms@gmail.com>
2026-03-07 17:52:40 +0530

    Initial Commit

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,8 @@ app.use(cors({
   credentials: true
 }));
 
+app.options("*", cors())
+
 // Body parser middleware
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
